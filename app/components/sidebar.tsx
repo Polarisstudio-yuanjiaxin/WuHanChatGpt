59b7dc19853f6eafc7bd5f9e1097ddbf6f3de739
北极星 <3494411683@qq.com>
2023-06-02 13:36:31 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -31,7 +31,16 @@ import { showToast } from "./ui-lib";
 const ChatList = dynamic(async () => (await import("./chat-list")).ChatList, {
   loading: () => null,
 });
+<script type="text/javascript" charset="utf-8" async=""
+  src="https://cdn.jsdelivr.net/npm/live2d-widget@3.1.4/lib/L2Dwidget.0.min.js">
+</script>
 
+<script type="text/javascript"
+  src="https://cdn.jsdelivr.net/npm/live2d-widget@3.1.4/lib/L2Dwidget.min.js?_=1557308476616"></script>
+
+<script type="text/javascript">
+  L2Dwidget.init();
+</script>
 function useHotKey() {
   const chatStore = useChatStore();
 
